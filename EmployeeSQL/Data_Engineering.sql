9299b8ff5a45619f223b2a8e701d5c293fffe256
lhenry97 <lhenry.lauren@gmail.com>
2024-07-21 12:40:21 +1000

    updated the data engineering file

--- EmployeeSQL/Data_Engineering.sql
+++ EmployeeSQL/Data_Engineering.sql
@@ -1,34 +1,49 @@
+-- Create tables for each CSV file
+CREATE TABLE titles(
+	title_id VARCHAR(255) PRIMARY KEY NOT NULL,
+	title VARCHAR(255)
+);
+
 CREATE TABLE employees(
 	employee_no INT PRIMARY KEY NOT NULL,
-	title_id VARCHAR,
-	birth_date VARCHAR,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date VARCHAR
+	employee_title_id VARCHAR(255),
+	birth_date VARCHAR(255),
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	sex VARCHAR(255),
+	hire_date VARCHAR(255),
+	FOREIGN KEY (employee_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE salaries(
 	employee_no INT NOT NULL,
-	salary INT
+	salary INT,
+	FOREIGN KEY (employee_no) REFERENCES employees(employee_no)
+);
+
+CREATE TABLE departments(
+	department_no VARCHAR(255) PRIMARY KEY NOT NULL,
+	department_name VARCHAR(255)
+);
+
+CREATE TABLE dept_manager(
+	department_no VARCHAR(255) NOT NULL,
+	employee_no INT NOT NULL,
+	FOREIGN KEY (employee_no) REFERENCES employees(employee_no),
+	FOREIGN KEY (department_no) REFERENCES departments(department_no)
 );
 
 CREATE TABLE dept_emp(
 	employee_no INT NOT NULL,
-	department_no VARCHAR
+	department_no VARCHAR(255) NOT NULL,
+	FOREIGN KEY (employee_no) REFERENCES employees(employee_no),
+	FOREIGN KEY (department_no) REFERENCES departments(department_no)
 );
+-- Manually import each csv to relevant table (importing order will need to start with titles, employees & departments to ensure no failures)
+--Add serial id for tables without primary keys
 
-CREATE TABLE departments(
-	department_no VARCHAR PRIMARY KEY NOT NULL,
-	department_name VARCHAR
-);
+ALTER TABLE salaries ADD COLUMN id SERIAL PRIMARY KEY;
 
-CREATE TABLE dept_manager(
-	department_no VARCHAR,
-	employee_no INT
-);
+ALTER TABLE dept_emp ADD COLUMN id SERIAL PRIMARY KEY;
 
-CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY NOT NULL,
-	title VARCHAR
-);
+ALTER TABLE dept_manager ADD COLUMN id SERIAL PRIMARY KEY;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sql-challenge
+The following code was complete using pgAdmin to conduct an analysis on employee data of Pewlett Hackard in the 1980s and 1990s. 
+An ERD was created to sketch out how best to link the data sets.
+Data engineering was conducted in which tables were created and the csv data imported.
+Data Analysis was conducted by completeing a number of queries around salaries, departments, managers and employee identification information.
+The following reference was used alongside the class activities data:
+https://stackoverflow.com/questions/2944499/how-to-add-an-auto-incrementing-primary-key-to-an-existing-table-in-postgresql
